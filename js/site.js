937b95a664de6f9f7fc158e83e8ed703ac1a80fe
Bima Arafah <bima.arafah@gmail.com>
2014-11-25 13:49:34 +0700

    Site updated to 17f88bf

---
+++ js/site.js
@@ -0,0 +1,19 @@
+
+// get all plugins required for document, done!
+
+
+// write down your code below this line
+// ++++++++++++++++++++++++++++++++++++++++++
+
+;(function ($) {
+
+  $(function () {
+
+
+    var $window = $(window);
+    var $body   = $(document.body);
+
+
+  });
+
+}(window.jQuery));
